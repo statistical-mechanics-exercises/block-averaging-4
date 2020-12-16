2112f4ce491144558a433f29a6b1a2207148fb9c
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 19:09:41 +0000

    Added answer

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ eng = np.loadtxt("energies")[:,1]
 blocks = np.zeros(10)
 
 # Your code goes here
+k=0
+for i in range( len(eng) ) :
+  blocks[k] = blocks[k] + eng[i] 
+  if (i+1)%100==0 and i>0 : 
+    blocks[k] = blocks[k] / 100
+    k = k + 1
+  
+mean, sq = 0, 0  
+for bb in blocks : mean, sq = mean + bb, sq + bb*bb
+average, sq = mean / len(blocks), sq / len(blocks)
+var = ( len(blocks) / ( len(blocks) - 1 ) ) * ( sq - average*average )
+error = np.sqrt( var / len(blocks) )
+print( average, error )
